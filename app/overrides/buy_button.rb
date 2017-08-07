d6c49e899bbcbe811c070d724fcb14e61f8f191f
Christian Finck <christian@finck.at>
2017-08-07 17:16:40 +0200

    added link to tos text

--- app/overrides/buy_button.rb
+++ app/overrides/buy_button.rb
@@ -2,4 +2,4 @@ Deface::Override.new(:virtual_path => "spree/checkout/_payment",
                      :replace_contents => "[data-hook='buttons']",
                      :partial => "/store/buy_button",
                      :name => "buy_button",
-                     :disabled => Spree::Config[:always_include_confirm_step] ? true : false)
+                     :disabled => !Spree::Config[:always_include_confirm_step])
